9647fe56e3b795c0511afa55fe71553ef75f5b67
20centcroak <vpaveau@outook.com>
2020-12-03 14:53:04 +0100

    v2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyRevealjs", 
-    version="1.0",
+    version="2.0",
     author="20centCroak",
     author_email="",
     description="revealjs based presentation",
